1307c0ec92107bcbfac6a975b0e0b4e70bfe7976
Mohammed HAJJARI <medhajjari7@gmail.com>
2023-05-19 10:34:42 +0000

    Missing values

---
+++ models/staging/BMO/Missing_values_handling.sql
@@ -0,0 +1,24 @@
+
+WITH source_data AS (
+  SELECT *
+  FROM BMO.PUBLIC."main_oeuvre"
+),
+
+-- Replace missing values with accurate data
+cleaned_data AS (
+  SELECT
+    "annee",BE20,NOMBE20,"Famille_met","Lbl_fam_met","metier","nommetier","Dept","NomDept",
+    CASE WHEN "met" = '*' THEN NULL ELSE "met" END AS "met",
+    CASE WHEN "xmet" = '*' THEN NULL ELSE "xmet" END AS "xmet",
+    CASE WHEN "smet" = '*' THEN NULL ELSE "smet" END AS "smet", 
+    REG NUMBER,
+    NOM_REG
+  FROM source_data
+)
+
+SELECT *
+FROM cleaned_data;
+
+
+
+
