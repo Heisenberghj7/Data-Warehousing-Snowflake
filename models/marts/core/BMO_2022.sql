8a66a6a295f93e3017bcec57b60fbb61ae9e8265
Mohammed HAJJARI <medhajjari7@gmail.com>
2023-05-21 11:24:58 +0000

    core_tables_BMO

---
+++ models/marts/core/BMO_2022.sql
@@ -0,0 +1,39 @@
+-- models/eliminate_duplicates.sql
+
+-- Create a model to eliminate duplicate values based on specific columns and keep only one occurrence
+
+-- Step 1: Assign row numbers to each record within its partition
+WITH deduplicated AS (
+  SELECT
+    ROW_NUMBER() OVER (PARTITION BY BE22, "Famille_met", "Nom métier BMO", "Dept","met","xmet","smet" ORDER BY BE22) AS row_num,
+    *
+  FROM BMO.PUBLIC.BMO_2022
+),
+-- Step 2: Filter out duplicate records and keep only one occurrence
+filtered_data AS (
+  SELECT *
+  FROM deduplicated
+  WHERE row_num = 1
+),
+
+cleaned_data AS (
+  SELECT
+    "annee",
+    BE22,
+    NOMBE22,
+    "Famille_met",
+    "Lbl_fam_met",
+    "Code métier BMO",
+    "Nom métier BMO",
+    TRY_CAST("Dept" AS NUMBER) AS "Dept",
+    "NomDept",
+    TRY_CAST("met" AS NUMBER) AS "met",
+    TRY_CAST("xmet" AS NUMBER) AS "xmet",
+    TRY_CAST("smet" AS NUMBER) AS "smet",
+    REG ,
+    NOM_REG
+  FROM filtered_data
+)
+
+SELECT *
+FROM cleaned_data
